ced23fb8bfb3a83fed85fd24f36c09fbc314b802
huangzulin <hzulin@gmail.com>
2015-06-05 10:44:58 +0800

    抽象工厂模式
    添加 抽象工厂模式

---
+++ DesignPattern/Factory/AbstractFactory.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Factory
+{
+    public interface IFactory
+    {
+        IProduct CreateProduct();
+    }
+
+    public interface IProduct
+    {
+        void Show();
+    }
+
+    public class ProductA : IProduct
+    {
+        public override void Show()
+        {
+            Console.WriteLine("Product A");
+        }
+    }
+
+    public class ProductB : IProduct
+    {
+        public override void Show()
+        {
+            Console.WriteLine("Product B");
+        }
+    }
+    public class FactoryA : IFactory
+    {
+
+        public override IProduct CreateProduct()
+        {
+            return new ProductA();
+        }
+    }
+
+    public class Client
+    {
+        public void OrderProduct()
+        {
+            IFactory factory = new FactoryA();
+            factory.CreateProduct();
+        }
+    }
+}
